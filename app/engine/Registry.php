22fed56961e2b6abca1f93f9b926df3ab16e37ea
werwolf41 <werwolf41@gmail.com>
2017-05-25 15:32:54 +0300

    create registry class and change composer.json

---
+++ app/engine/Registry.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krasn
+ * Date: 25.05.2017
+ * Time: 15:23
+ */
+
+namespace app\engine;
+
+
+class Registry
+{
+    private $registry = [];
+
+    /**
+     * @param $key
+     * @return mixed|null
+     */
+    public function getRegistry($key)
+    {
+        return isset($this->registry[$key])? $this->registry[$key]: null;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function setRegistry($key, $value)
+    {
+        $this->registry[$key] = $value;
+    }
+
+
+}
--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,2 +1,10 @@
 <?php
+use app\engine\Registry;
+
 require_once __DIR__."/../../vendor/autoload.php";
+
+//Registry
+$registry = new Registry();
+$registry->setRegistry('test', 'test');
+
+echo $registry->getRegistry('test');
